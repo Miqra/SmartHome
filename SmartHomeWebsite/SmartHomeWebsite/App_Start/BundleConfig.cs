7f5ad1b3308d20c91716c61485b94b86a1db810f
Miqra <wrestling-88@mail.ru>
2016-10-24 19:04:40 +0400

    SmartHome

--- SmartHomeWebsite/SmartHomeWebsite/App_Start/BundleConfig.cs
+++ SmartHomeWebsite/SmartHomeWebsite/App_Start/BundleConfig.cs
@@ -15,7 +15,21 @@ namespace SmartHomeWebsite.App_Start
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/style.css"));
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-3.1.1.js"));
+                        "~/Scripts/jquery-3.1.1.js",
+                        "~/Scripts/jquery-1.9.1.intellisense.js",
+                        "~/Scripts/jquery-1.9.1.min.js",
+                        "~/Scripts/jquery-1.9.1.js",
+                        "~/Scripts/"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                        "~/Scripts/bootstrap.js",
+                        "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include(
+                      "~/Content/bootstrap.css",
+                       "~/Content/bootstrap-theme.css",
+                       "~/Content/bootstrap-theme.min.css",
+                       "~/Content/bootstrap.min.css"));
 
         }
     }
---
+++ SmartHomeWebsite/SmartHomeWebsite/Crypto.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SmartHomeWebsite
+{
+    public class Crypto
+    {
+        public string Crypt(string code)
+        {
+            try
+            {
+                string key = "Ismayil";
+
+                MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+                SHA1CryptoServiceProvider hashSHA1 = new SHA1CryptoServiceProvider();
+                TripleDESCryptoServiceProvider myTripleDES = new TripleDESCryptoServiceProvider();
+                string varSHA1 = Convert.ToBase64String(hashSHA1.ComputeHash(ASCIIEncoding.ASCII.GetBytes(key)));
+                myTripleDES.Key = hashMD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(varSHA1));
+                myTripleDES.Mode = CipherMode.ECB;
+                ICryptoTransform desdencrypt = myTripleDES.CreateEncryptor();
+                ASCIIEncoding MyASCIIEncoding = new ASCIIEncoding();
+                byte[] buff = ASCIIEncoding.ASCII.GetBytes(code);
+                return Convert.ToBase64String(desdencrypt.TransformFinalBlock(buff, 0, buff.Length));
+            }
+            catch
+            {
+
+                return "";
+            }
+        }
+
+
+        public string DeCrypt(string code)
+        {
+            try
+            {
+                string key = "Ismayil";
+
+                MD5CryptoServiceProvider hashMD5 = new MD5CryptoServiceProvider();
+                SHA1CryptoServiceProvider hashSHA1 = new SHA1CryptoServiceProvider();
+                TripleDESCryptoServiceProvider myTripleDES = new TripleDESCryptoServiceProvider();
+                string varSHA1 = Convert.ToBase64String(hashSHA1.ComputeHash(ASCIIEncoding.ASCII.GetBytes(key)));
+                myTripleDES.Key = hashMD5.ComputeHash(ASCIIEncoding.ASCII.GetBytes(varSHA1));
+                myTripleDES.Mode = CipherMode.ECB;
+                ICryptoTransform desdencrypt = myTripleDES.CreateDecryptor();
+                byte[] buff = Convert.FromBase64String(code);
+                return ASCIIEncoding.ASCII.GetString(desdencrypt.TransformFinalBlock(buff, 0, buff.Length));
+            }
+            catch
+            {
+
+                return "";
+            }
+
+        }
+    }
+}
